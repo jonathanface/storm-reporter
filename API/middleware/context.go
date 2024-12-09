69b641d492ce39162c2e8b04e079980db72d6ea6
Jonathan Face <jonathan.face@protonmail.com>
2024-12-09 12:28:36 -0600

    commentary

--- API/middleware/context.go
+++ API/middleware/context.go
@@ -12,6 +12,7 @@ type contextKey string
 const daoKey contextKey = "dao"
 
 func WithDAOContext(dao *dao.StormDAO) func(http.HandlerFunc) http.HandlerFunc {
+	// Normally would put some kind of auth here, but for now just return a middleware that sets the DAO in the context
 	return func(next http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
 			ctx := context.WithValue(r.Context(), daoKey, dao)
